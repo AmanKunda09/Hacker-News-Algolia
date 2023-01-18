02636e1f836c7d76ad5c22cadedc8afa53697dab
Aman <amankunda9@gmail.com>
2023-01-18 09:49:14 +0530

    adding the fetch stories functionality and styles

--- src/components/Stories.js
+++ src/components/Stories.js
@@ -1,7 +1,46 @@
 import React from "react";
 
+import { useGlobalContext } from "../context";
+
 const Stories = () => {
-  return <div>Stories</div>;
+  const { isLoading, hits, removeStory } = useGlobalContext();
+
+  if (isLoading) {
+    return <div className="loading"></div>;
+  }
+  return (
+    <section className="stories">
+      {hits.map((story) => {
+        const { objectID, title, author, url, num_comments, points } = story;
+
+        return (
+          <article className="story" key={objectID}>
+            <h4 className="title">{title}</h4>
+            <p className="info">
+              {points} points by <span>{author} | </span> {num_comments}{" "}
+              comments
+            </p>
+            <div>
+              <a
+                href={url}
+                className="read-link"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                read more
+              </a>
+              <button
+                className="remove-btn"
+                onClick={() => removeStory(objectID)}
+              >
+                remove
+              </button>
+            </div>
+          </article>
+        );
+      })}
+    </section>
+  );
 };
 
 export default Stories;
